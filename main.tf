51e48e7463a1100ee61ce4675c0a085d82c46122
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-11 05:58:23 +0000

    using naming mod for rg creation

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 module "tf-az-rg" {
-  source      = "app.terraform.io/traindevsecops/tf-az-rg/azure"
-  version     = "1.0.1"
-  env         = "project1-dev"
-  gen_rand_id = "yyll"
+  source              = "app.terraform.io/traindevsecops/tf-az-rg/azure"
+  version             = "1.2.0"
+  resource_group_name = module.tf-az-naming-convention.service_name["resource_group_name"]
+  # insert required variables here
+}
+
+module "tf-az-naming-convention" {
+  source       = "app.terraform.io/traindevsecops/tf-az-naming-convention/azure"
+  version      = "1.0.1"
+  project_name = "yest"
+  environment  = "prod"
 }
